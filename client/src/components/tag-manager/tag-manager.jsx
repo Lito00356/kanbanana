a25a9a05a79f470614f1927aa141509171598877
lito00356 <tomasz.liksza@gmail.com>
2026-07-22 16:36:16 +0200

    Changed css for menu bar and modal text color

--- client/src/components/tag-manager/tag-manager.jsx
+++ client/src/components/tag-manager/tag-manager.jsx
@@ -33,8 +33,10 @@ export function TagManager({ tags = [], onAddTag, onDeleteTag }) {
 
   return (
     <>
-      <button className="button tag-manager__toggle" onClick={openDialog}>
-        Manage Tags
+      <button className="button tag-manager__toggle" onClick={openDialog} title="Manage Tags">
+        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="tag-manager__icon">
+          <path d="M7.5 8.5H7.51M3 12.5L9.39 18.89C9.83 19.33 10.05 19.55 10.3 19.63C10.52 19.7 10.76 19.7 10.98 19.63C11.23 19.55 11.45 19.33 11.89 18.89L20 10.78C20.45 10.33 20.67 10.11 20.79 9.85C20.9 9.62 20.95 9.37 20.95 9.11V5.2C20.95 4.08 20.95 3.52 20.73 3.09C20.54 2.72 20.23 2.41 19.86 2.22C19.43 2 18.87 2 17.75 2H13.84C13.58 2 13.33 2.05 13.1 2.16C12.84 2.28 12.62 2.5 12.17 2.95L3 12.5Z" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
       </button>
 
       <dialog className={`modal tag-manager__modal ${isOpen ? "open" : ""}`} ref={dialogRef}>
