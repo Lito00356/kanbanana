4f8f809b2653af8c3227286a6628d87e370e5fa5
lito00356 <tomasz.liksza@gmail.com>
2026-07-29 16:56:10 +0200

    added render api to constants

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,2 +1,2 @@
-export const API_URL = import.meta.env.VITE_API_URL;
+export const API_URL = import.meta.env.VITE_API_URL || "https://kanbanana-w7vi.onrender.com/api";
 export const API_TOKEN = import.meta.env.VITE_API_TOKEN;
